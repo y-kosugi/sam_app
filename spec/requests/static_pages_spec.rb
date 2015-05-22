4afcb3a7e3f30daf1e1e92766eb4112813055de0
yuto <your.email@example.com>
2015-05-22 11:07:06 +0900

    test2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,5 +7,13 @@ describe "StaticPages" do
      visit '/static_pages/home'
      expect(page).to have_content('Sample App')
    end
- end  
+ end
+
+ describe "Help page" do
+
+   it "should have the content 'Help'" do
+     visit '/static_pages/help'
+     expect(page).to have_content('Help')
+   end
+ end
 end
