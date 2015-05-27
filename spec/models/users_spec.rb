03db3d80b5d982a3771ef54e4e066548d11639cc
yuto <your.email@example.com>
2015-05-27 10:26:38 +0900

    user4

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
-describe Users do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
 end
